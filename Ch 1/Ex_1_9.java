f20e6bc950a974af6482e0011fb0a2f3af5e2fdb
Kaede <kaedelynn84720@gmail.com>
2018-10-11 09:34:19 -0600

    Added Ch 1 Assignments

---
+++ Ch 1/Ex_1_9.java
@@ -0,0 +1,13 @@
+/*
+Author: Kaede Cunningham
+Date: 9/27/18
+
+Compute expression
+*/
+
+class Ex_1_9 {
+	public static void main(String[] args) {
+		System.out.println( 4.5 * 7.9 );
+		System.out.print(2*4.5 + 2*7.9);
+	}
+}
