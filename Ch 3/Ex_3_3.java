a39b9f18de3205abcfb96535f73038a27f690e0f
Kaede Cunningham <student@HSiMac4-8.local>
2018-10-17 11:32:25 -0600

    Added Ch 3 assignments

---
+++ Ch 3/Ex_3_3.java
@@ -0,0 +1,18 @@
+/*
+Author: Kaede Cunningham
+Date: 10/17/18
+
+
+*/
+class 3_3 {
+	public static void main(String[] args) {
+			// Create a Scanner
+			Scanner input = new Scanner(System.in);
+			
+			// Receive an input
+			System.out.print("Enter an integer: ");
+			int number = input.nextInt();
+			
+			if 		
+	}
+}
