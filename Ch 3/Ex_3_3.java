e9fc823a55a2b4ed00d5ac8031120e45d66670cd
Kaede Cunningham <student@HSiMac4-8.local>
2018-10-24 11:39:50 -0600

    Added to Ch 3

--- Ch 3/Ex_3_3.java
+++ Ch 3/Ex_3_3.java
@@ -1,18 +1,19 @@
 /*
 Author: Kaede Cunningham
-Date: 10/17/18
-
+Date: 10/24/18
 
 */
 class 3_3 {
 	public static void main(String[] args) {
-			// Create a Scanner
-			Scanner input = new Scanner(System.in);
-			
-			// Receive an input
-			System.out.print("Enter an integer: ");
-			int number = input.nextInt();
-			
-			if 		
+		//declare variable
+		Scanner input = new Scanner(System.in);
+		double a = 0.0;
+		double b = 0.0;
+		double c = 0.0;
+		double d = 0.0;
+		double e = 0.0;
+		double f = 0.0;
+		double x = 0.0;
+		double y = 0.0;
 	}
 }
