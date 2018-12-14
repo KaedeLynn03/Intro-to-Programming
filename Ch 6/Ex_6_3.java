d84d9b7657b99ef77c808ad9b6a9bac83c17a368
Kaede Cunningham <kaedelynn84720@gmail.com>
2018-12-14 10:14:31 -0700

    added to ch 6

---
+++ Ch 6/Ex_6_3.java
@@ -0,0 +1,14 @@
+/*
+Author: Kaede Cunningham
+Date: 12/14/18
+
+*/
+
+class Ex_6_3 {
+	public static void main(String[] args) {
+		// Return the reversal of an integer, i.e., reverse (456) returns 654
+		public static int reverse(int number);
+		// Return true if number is a palidrome
+		public static boolean isPalindrome(int number);
+	}
+}
