1f2bad9e36347e506ff49e9d4d18859a2fd40936
Kaede Cunningham <kaedelynn84720@gmail.com>
2019-01-11 09:42:02 -0700

    Edited and kinda fixed this program

--- Ch 6/Ex_6_3.java
+++ Ch 6/Ex_6_3.java
@@ -3,12 +3,34 @@ Author: Kaede Cunningham
 Date: 12/14/18
 
 */
+import java.util.Scanner;
 
 class Ex_6_3 {
 	public static void main(String[] args) {
-		// Return the reversal of an integer, i.e., reverse (456) returns 654
-		public static int reverse(int number);
-		// Return true if number is a palidrome
-		public static boolean isPalindrome(int number);
+		Scanner input = new Scanner(System.in);
+		int user = 0;
+		int num = 0;
+		int remain = 0;
+		int reverse = 0;
+		
+		// Prompt user 
+		System.out.print("Enter a number: ");
+		user = input.nextInt();
+		
+		
+		// Copy user to a mutable variable 
+		num = user;
+		while (num != 0) {
+			remian = num % 10;
+			reverse = (reverse * 10) + remain; 
+			num /= 10;
+		}
+		
+		if (user == reverse) {
+			System.out.println(user + "is a Palendrome");
+		}
+		else{
+			System.out.println(user + "is not a Palendrome");
+		}
 	}
 }
