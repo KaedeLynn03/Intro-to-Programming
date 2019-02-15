62a8cc4934308f9c10f6ae942cb2fd2b13fa1803
Kaede Cunningham <kaedelynn84720@gmail.com>
2019-02-15 10:01:59 -0700

    added

--- Ch 7 /Ex_7_23.java
+++ Ch 7 /Ex_7_23.java
@@ -1,10 +1,13 @@
 /*
 Author: Kaede Cunningham
-Date: 2/13/19
+Date: 2/15/19
 
 */
+import java.util.Scanner;
+
 class Ex_7_23 {
 	public static void main(String[] args) {
-		
+		//Declare Scanner and array
+		Scanner input = new Scanner(System.in);
 	}
 }
