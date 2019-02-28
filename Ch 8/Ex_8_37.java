adc95a3c70ba407680e8f440ccd7fc6acd34468e
Kaede Cunningham <kaedelynn84720@gmail.com>
2019-02-28 10:07:21 -0700

    Added to Ch 8

---
+++ Ch 8/Ex_8_37.java
@@ -0,0 +1,12 @@
+/*
+Author: Kaede Cunningham
+Date: 2/28/19
+
+*/
+class Ex_8_37 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String[][] quiz = new quiz {{"Utah"}}
+		
+	}
+}
