ba00f6aa45df86e24099f1c4751bf0187005c9e0
Kaede Cunningham <kaedelynn84720@gmail.com>
2019-04-30 10:03:47 -0600

    Put code on

--- End Of Year Project/DiceRoller.java
+++ End Of Year Project/DiceRoller.java
@@ -3,8 +3,20 @@ Author: Kaede Cunningham
 
 project: Dice Roller
 */
+
+import java.util.Scanner;
 class DiceRoller {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		double dice, sides;
 		
+		System.out.print("Enter the number of dice desired: ");
+		dice = input.nextDouble();
+		
+		System.out.print("Enter the number of sides desired on dice: ");
+		sides = input.nextDouble();
+		
+		int number = (int)(Math.random() * (sides + 1));
+		System.out.print("Your numbers are:" + number);
 	}
 }
